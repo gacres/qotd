cf1a9af21cf9ee40331d31af7606f29568ab4410
Graham Acres <graham.acres@brytek.ca>
2019-04-20 15:29:32 -0700

    Update QotdList.js
    Trying to get the List of questions to display.  Something is still not clicking.

--- src/components/qotd/QotdList.js
+++ src/components/qotd/QotdList.js
@@ -1,47 +1,35 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import Table from 'react-bootstrap/Table';
 import { fetchQuestions } from '../../actions';
 
 
 class QotdList extends React.Component {
     componentDidMount() {
+        console.log("QotdList.componentDidMount");
         this.props.fetchQuestions();
+        console.log(this.props.questions);
+    }
+
+    renderList() {
+        return this.props.questions.map(question => {
+            return (
+                <div id={question.QuestionKey}>
+                    <label>QuestionText</label>
+                    {question.QuestionText}
+                    <div>
+                        <label>QuestionDescription</label>
+                        {question.QuestionDescription}
+                    </div>
+                </div>
+            );
+        });
     }
 
     render() {
         return (
             <div>
-                <br />
-                <Table striped bordered hover>
-                    <thead>
-                        <tr>
-                        <th>#</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Username</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                        </tr>
-                        <tr>
-                            <td>3</td>
-                            <td colSpan="2">Larry the Bird</td>
-                            <td>@twitter</td>
-                        </tr>
-                    </tbody>
-                </Table>
+                <h2>Previous Questions</h2>
+                <div>{this.renderList()}</div>
             </div>
         );
     }
